4861b3f22c70e2042848bf86152b91a604203cc3
Nitish Kumar <nitishkumar@Nitishs-MacBook-Air.local>
2026-05-12 10:49:47 +0530

    fixed the _init_.py version

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,5 @@ Mac Deep Cleaner v1.5.0 — Professional Edition
 Smart App Orphan Detector & System Cleanup Tool for macOS
 """
 
-__version__ = "1.2.0"
+__version__ = "1.5.0"
 __author__ = "NK2552003"
